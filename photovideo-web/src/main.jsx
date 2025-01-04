4d78a3bc163363aed00dc5fb06b1d9e2ffac3b6b
Hannahphoto <hannahprice67@gmail.com>
2025-01-04 11:58:08 -0500

    main file completed

--- photovideo-web/src/main.jsx
+++ photovideo-web/src/main.jsx
@@ -5,8 +5,51 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import './index.css';
 
+//bring pages so router views correct page
+
+import ErrorPage from './pages/ErrorPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import PhotosPage from './pages/PhotosPage.jsx';
+import VideosPage from './pages/VideosPage.jsx';
+import CommissionsPage from './pages/CommissionPage.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+import CvPage from './pages/CvPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage/>,
+    errorElement: <ErrorPage/>,
+  },
+  {
+    path: '/',
+    element: <PhotosPage/>,
+    errorElement: <ErrorPage/>,
+  },
+  {
+    path: '/',
+    element: <VideosPage/>,
+    errorElement: <ErrorPage/>,
+  },
+  {
+    path: '/',
+    element: <CommissionsPage/>,
+    errorElement: <ErrorPage/>,
+  },
+  {
+    path: '/',
+    element: <ContactPage/>,
+    errorElement: <ErrorPage/>,
+  },
+  {
+    path: '/',
+    element: <CvPage/>,
+    errorElement: <ErrorPage/>,
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  /* </React.StrictMode>, */
 )
