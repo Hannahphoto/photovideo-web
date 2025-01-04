2c8e3076318de921e97d17a896c9b98398005854
Hannahphoto <hannahprice67@gmail.com>
2025-01-04 17:33:14 -0500

    footer and colors

--- photovideo-web/src/components/navbar.jsx
+++ photovideo-web/src/components/navbar.jsx
@@ -5,19 +5,22 @@ export default function Navbar({ }) {
     console.log('Navbar Rendered');
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ padding: "0px" }} >
+            <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ padding: "0px", margin: "1.5%" }} >
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#" style={{ color: 'white' }}  >Hannah Price</a>
+                    <a className="navbar-brand poppins-medium" href="#" style={{ color: '#cbbbe3', fontSize: '4vw' }}  >Hannah Price</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#" style={{ color: 'white' }}>Photos</a>
+                                <a className="nav-link" aria-current="page" href="#" style={{ color: 'white' }}>Photos</a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#" style={{ color: 'white' }}>Videos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" style={{ color: 'white' }}>Commissions</a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#" style={{ color: 'white' }}>CV</a>
@@ -29,6 +32,11 @@ export default function Navbar({ }) {
                     </div>
                 </div>
             </nav>
+            <footer className="fixed-bottom">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#" style={{ color: 'white' }}>Footer</a>
+                </div>
+            </footer>
         </>
     )
 
--- photovideo-web/src/components/navigate.jsx
+++ photovideo-web/src/components/navigate.jsx
@@ -1,2 +1,42 @@
-// import {useState} from 'react'
-// import HomePage from '../pages/HomePage'
+import { useState } from "react";
+import HomePage from '../pages/HomePage'
+import PhotosPage from '../pages/PhotosPage'
+import VideosPage from '../pages/VideosPage'
+import CommissionsPage from '../pages/CommissionsPage'
+import CvPage from '../pages/CvPage'
+import ContactPage from '../pages/ContactPage'
+
+export default function NavigateSite(){
+    const [currentPage, setCurrentPage] = useState('HomePage');
+
+     // depending on the value of the currentPage we return the responding component to render
+     const renderPage = () => {
+        if(currentPage === 'Home'){
+            return <HomePage/>;
+        }
+        if(currentPage === 'Photos'){
+            return <PhotosPage/>;
+        }
+        if(currentPage === 'Videos'){
+            return <VideosPage/>;
+        }
+        if(currentPage === 'Commissions'){
+            return <CommissionsPage/>;
+        }
+        if(currentPage === 'Contact'){
+            return <ContactPage/>;
+        }
+        if(currentPage === 'CV'){
+            return <CvPage/>;
+        }
+    }
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return(
+
+    )
+}
+
+
+
