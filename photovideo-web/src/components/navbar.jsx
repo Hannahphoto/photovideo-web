6198f298da75f72b4bf79068a48935355999dbc7
Hannahphoto <hannahprice67@gmail.com>
2025-01-04 15:24:58 -0500

    navbar where i want

--- photovideo-web/src/components/navbar.jsx
+++ photovideo-web/src/components/navbar.jsx
@@ -5,11 +5,11 @@ export default function Navbar({}) {
     console.log('Navbar Rendered');
     return (
         <>
-             <nav className="navbar navbar-expand-lg bg-body-tertiary">
+             <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{padding:"0px"}} >
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#" >Hannah Price</a>
+                        <a className="navbar-brand" href="#"style={{color:'white'}}  >Hannah Price</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
+                            <span className="navbar-toggler-icon" ></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarNav">
                             <ul className="navbar-nav">
