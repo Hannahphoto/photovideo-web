9dcfd7b49e09eefd07e0bec7407dd7c39fa1b1ba
Hannahphoto <hannahprice67@gmail.com>
2025-01-04 15:35:52 -0500

    finally got the beginning where i want it

--- photovideo-web/src/components/navbar.jsx
+++ photovideo-web/src/components/navbar.jsx
@@ -1,34 +1,34 @@
 
 import { Link } from "react-router-dom"
 
-export default function Navbar({}) {
+export default function Navbar({ }) {
     console.log('Navbar Rendered');
     return (
         <>
-             <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{padding:"0px"}} >
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#"style={{color:'white'}}  >Hannah Price</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon" ></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">Photos</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Videos</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">CV</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link disabled" aria-disabled="true">Contact</a>
-                                </li>
-                            </ul>
-                        </div>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ padding: "0px" }} >
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#" style={{ color: 'white' }}  >Hannah Price</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#" style={{ color: 'white' }}>Photos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" style={{ color: 'white' }}>Videos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" style={{ color: 'white' }}>CV</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" style={{ color: 'white' }}>Contact</a>
+                            </li>
+                        </ul>
                     </div>
-                </nav>
+                </div>
+            </nav>
         </>
     )
 
