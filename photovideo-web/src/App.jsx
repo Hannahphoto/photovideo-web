1af61d8a634a2a93d23fa308b799c6847d931da1
Hannahphoto <hannahprice67@gmail.com>
2025-01-04 14:44:40 -0500

    getting somewhere, navbar and font, bootstrap working

--- photovideo-web/src/App.jsx
+++ photovideo-web/src/App.jsx
@@ -11,38 +11,26 @@ import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                      Nav Bar Test
-                      <div className="container-fluid">
-                          <a className="navbar-brand" href="#">Hannah Price</a>
-                          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                              <span className="navbar-toggler-icon"></span>
-                          </button>
-                          <div className="collapse navbar-collapse" id="navbarNav">
-                              <ul className="navbar-nav">
-                                  <li className="nav-item">
-                                      <a className="nav-link active" aria-current="page" href="#">Photos</a>
-                                  </li>
-                                  <li className="nav-item">
-                                      <a className="nav-link" href="#">Videos</a>
-                                  </li>
-                                  <li className="nav-item">
-                                      <a className="nav-link" href="#">CV</a>
-                                  </li>
-                                  <li className="nav-item">
-                                      <a className="nav-link disabled" aria-disabled="true">Contact</a>
-                                  </li>
-                              </ul>
-                          </div>
-                      </div>
-                  </nav>
-     
-    </>
-  )
+    return (
+        <>
+            <html>
+                <head>
+                    <script src="http://localhost:5173"></script>
+                </head>
+               <Navbar/>
+               <Footer/>
+               <Home/>
+               <Photos/>
+               <Videos/>
+               <Commissions/>
+               <Contact/>
+               <CV/>
+
+            </html>
+        </>
+    )
 }
 
 export default App
