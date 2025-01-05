1a1f706bb1ad7344cacdc15bcd5708bb30ea67f7
Hannahphoto <hannahprice67@gmail.com>
2025-01-05 18:49:41 -0500

    clean

--- photovideo-web/src/components/navigate.jsx
+++ photovideo-web/src/components/navigate.jsx
@@ -35,18 +35,7 @@ export default function NavigateSite() {
     } 
   }
 
-  // return (
-  //   <nav className="navigation">
-  //     <ul>
-  //       <li><Link to="/">Home</Link></li>
-  //       <li><Link to="/photos">Photos</Link></li>
-  //       <li><Link to="/videos">Videos</Link></li>
-  //       <li><Link to="/commissions">Commissions</Link></li>
-  //       <li><Link to="/contact">Contact</Link></li>
-  //       <li><Link to="/cv">CV</Link></li>
-  //     </ul>
-  //   </nav>
-  // );
+
   const handlePageChange = (page) => setCurrentPage(page);
 
 
