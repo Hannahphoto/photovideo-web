7b60943fd62440ced5a2bd0b644fc5c7e7724a87
Hannahphoto <hannahprice67@gmail.com>
2025-01-05 22:11:37 -0500

    styled buttons

--- photovideo-web/src/components/videos.jsx
+++ photovideo-web/src/components/videos.jsx
@@ -11,35 +11,35 @@ export default function Videos() {
             <div className="row row-cols-1 row-cols-md-3 g-4">
                 <div className="card text-bg-dark">
                     <img src="/images/oct27.png" className="card-img" alt="..."></img>
-                        <div className="card-img-overlay">
-                            <h5 className="card-title">October 27th, 2020</h5>
-                          
-                         
-                        </div>
+                       {/* <div className="card-img-overlay">
+                            <h5 className="card-title"></h5>
+                            {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> 
+                            
+                            </div>*/}
+                        <button type="button" className="btn btn-outline-info"style={{ color: "white" }}>October 27th, 2020</button>
                 </div>
                 <div className="card text-bg-dark">
                     <img src="/images/blueprint.png" className="card-img" alt="..."></img>
-                        <div className="card-img-overlay">
+                        {/* <div className="card-img-overlay">
                             <h5 className="card-title">Blueprint</h5>
-                         
-                          
-                        </div>
+                        </div> */}
+                        <button type="button" className="btn btn-outline-info" style={{ color: "white" }}>Blueprint</button>
                 </div>
                 <div className="card text-bg-dark">
                     <img src="/images/barber.png" className="card-img" alt="..."></img>
-                        <div className="card-img-overlay">
+                        {/* <div className="card-img-overlay">
                             <h5 className="card-title">Revisited & Improvised</h5>
-                            {/* <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> */}
-                           
-                        </div>
+                             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> 
+                        </div> */}
+                        <button type="button" className="btn btn-outline-info"style={{ color: "white" }}>Revisited & Improvised</button>
                 </div>
                 <div className="card text-bg-dark">
                     <img src="/images/narrativeattempt.png" className="card-img" alt="..."></img>
-                        <div className="card-img-overlay">
+                        {/* <div className="card-img-overlay">
                             <h5 className="card-title">Untitled, An Attempt At Narrative</h5>
-                            {/* <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> */}
-                          
-                        </div>
+                             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> 
+                        </div> */}
+                        <button type="button" className="btn btn-outline-info"style={{ color: "white" }}>Untitled, An Attempt At Narrative</button>
                 </div>
                 {/* <div className="card text-bg-dark">
                     <img src="..." className="card-img" alt="..."></img>
