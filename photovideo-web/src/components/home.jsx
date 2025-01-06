fd8f1e8ad308db878b61aca5e5a1ecb83de06505
Hannahphoto <hannahprice67@gmail.com>
2025-01-06 17:31:46 -0500

    almost done

--- photovideo-web/src/components/home.jsx
+++ photovideo-web/src/components/home.jsx
@@ -1,32 +1,112 @@
 
 import { useEffect } from "react";
 
+
 export default function Home() {
-   
+
     return (
         <>
-            <div id="carouselExample" className="carousel slide">
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src="/images/kingsolomon.png" className="d-block w-100" alt="..."></img>
-                    </div>
-                    <div className="carousel-item">
-                        <img src="/images/semaphore.png" className="d-block w-100" alt="..."></img>
-                    </div>
-                    <div className="carousel-item">
-                        <img src="/images/highway.png" className="d-block w-100" alt="..."></img>
-                    </div>
+            <h1 className="--bs-heading-color" style={{ color: "white" }}>
+                Recent Book "Still Standing"
+            </h1>
+            <div className="card text-bg-dark">
+                <div className="card-body">
+                    <img src="/images/Book-png/book_01.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_02.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_03.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_04.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_05.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_06.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_07.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_08.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_09.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_10.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_11.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_12.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_13.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_14.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_15.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_16.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_17.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_18.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_19.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_20.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_21.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_22.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_23.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_24.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_25.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_26.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_27.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_28.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_29.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_30.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_31.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_32.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_33.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_34.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_35.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_36.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_37.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_38.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_39.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_40.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_41.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_42.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_43.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_44.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_45.png" className="d-block w-100" alt="..."></img>
+                    <br></br>
+                    <img src="/images/Book-png/book_46.png" className="d-block w-100" alt="..."></img>
+                   
                 </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
             </div>
         </>
+
     )
 
 }
