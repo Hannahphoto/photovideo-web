fdc7cc23a8c7f808ac8991ba868e855160ed1190
Hannahphoto <hannahprice67@gmail.com>
2025-01-05 20:52:22 -0500

    videos page comming along

--- photovideo-web/src/components/videos.jsx
+++ photovideo-web/src/components/videos.jsx
@@ -1,8 +1,52 @@
 
-export default function Videos(){
+export default function Videos() {
 
     return (
         <>
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                <div className="card text-bg-dark">
+                    <img src="/images/oct27.png" className="card-img" alt="..."></img>
+                        <div className="card-img-overlay">
+                            <h5 className="card-title">October 27th, 2020</h5>
+                          
+                         
+                        </div>
+                </div>
+                <div className="card text-bg-dark">
+                    <img src="/images/blueprint.png" className="card-img" alt="..."></img>
+                        <div className="card-img-overlay">
+                            <h5 className="card-title">Blueprint</h5>
+                         
+                          
+                        </div>
+                </div>
+                <div className="card text-bg-dark">
+                    <img src="/images/barber.png" className="card-img" alt="..."></img>
+                        <div className="card-img-overlay">
+                            <h5 className="card-title">Revisited & Improvised</h5>
+                            {/* <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> */}
+                           
+                        </div>
+                </div>
+                <div className="card text-bg-dark">
+                    <img src="/images/narrativeattempt.png" className="card-img" alt="..."></img>
+                        <div className="card-img-overlay">
+                            <h5 className="card-title">Untitled, An Attempt At Narrative</h5>
+                            {/* <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> */}
+                          
+                        </div>
+                </div>
+                {/* <div className="card text-bg-dark">
+                    <img src="..." className="card-img" alt="..."></img>
+                        <div className="card-img-overlay">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                           
+                        </div>
+                </div> */}
+
+
+            </div>
         </>
     )
 
