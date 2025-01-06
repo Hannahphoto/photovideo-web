6c0ec6116cd682f7601d5ba61eff8853b5028055
Hannahphoto <hannahprice67@gmail.com>
2025-01-05 23:47:19 -0500

    cv done

--- photovideo-web/src/components/cv.jsx
+++ photovideo-web/src/components/cv.jsx
@@ -1,8 +1,106 @@
 
-export default function cv(){
+export default function cv() {
 
     return (
         <>
+            <h1 className="--bs-heading-color" style={{ color: "white" }}>
+                CV
+            </h1>
+            <div className="card text-bg-dark">
+
+                <div className="card-body" style={{ color: "white" }} >
+                    <h3>
+                        EXHIBITIONS
+                    </h3>
+                    
+                    <p>
+                        <em>Hannah Price, Still Standing, African American Cultural Heritage Fund & National Trust of  Historic Preservation.</em> Silver Eye Center for Photography, The Aaronel deRay & Irving Gruber Gallery. December 2, 2023 - February 11, 2023.
+                    </p>
+                    
+                    <p>
+                    <em>Close Enough; New Perspectives of 12 Women from Magnum.</em> International Center of  Photography, New York, New York. September 29, 2022 - January 09, 2023. Curated by  Charlotte Cotton. Traveling show - Palau de les Comunicacions de Alicante,Plaza Gabriel Miro,  Alicant. Spain.
+                    </p>
+                    <p>
+                        <em>Semaphore.</em> Solo Exhibition at the Silver Eye Center for Photography, Pittsburgh, PA. September 6th, 2018 - November 17th, 2018.
+                    </p>
+                    <p>
+                        <em>Another Way of Telling: Women Photographers from the Collection.</em>
+                        Philadelphia Museum of Art, Philadelphia, PA. April 8th, 2017 - July 16th, 2017.
+                    </p>
+                    <p>
+                        <em>Magic Mirror. </em>Daniel Cooney Fine Art. June 2017.
+                    </p>
+                    <p>
+                        <em>2016 Contemporary Photography Solo Exhibition: Cursed by Night.</em> Philadelphia Photo Arts Center, Philadelphia, PA. December 8th, 2016 - February 25th, 2017.
+                    </p>
+                    <p>
+                        <em>City of Brotherly Love.</em> Perspectives Gallery, Milwaukee Institute of Art and Design.  October 31st - November 18th, 2016.
+                    </p>
+                    <p>
+                       <em>Anthology: Somewhere Not Here.</em> Crosstown Arts Gallery, Memphis, TN.
+                        August 19th - September 17th, 2016.
+                    </p>
+                    <p>
+                        <em>New Genealogies</em>, Green Gallery, Yale School of Art, New Haven, CT. January 2016.
+                    </p>
+                    <p>
+                        Video Screening of Attempt at Narrative. BombPop! Petes Candy Store. August, 2017.
+                    </p>
+                    <p>
+                        Video Screening and Discussion of Blueprint. Diverse Works, Houston, TX.
+                        December 13th, 2014.
+                    </p>
+                    <p>
+                        Deep End - Yale MFA Photography Thesis. Diane Rosenstein Gallery, Los Angeles, CA. July 19th - August 16th 2014.
+                    </p>
+                    <p>
+                        Deep End - Yale MFA Photography Thesis, Curated by Roe Ethridge. The FLAG Art  Foundation, New York, NY. June 5th - 20th 2014.
+                    </p>
+                    <p>
+                        <em>tete - a - tete</em>. Group Show Curated by Mickalene Thomas. QF Gallery, East Hampton, NY.  August 24 - October 14th 2013.
+                    </p>
+                    <p>
+                        <em>Here and Now: Prints, Drawings, and Photographs by Ten Philadelphia Artists.</em> Stieglitz Gallery, The Philadelphia Museum of Art, September 10th - December 4th, 2011.
+                    </p>
+                    <p>
+                       <em> (Por)trait Revealed.</em> RayKo Photo Center, San Francisco, CA. July 28- September 10, 2010.
+                    </p>
+                    <p>
+                        <em>In Review.</em> Gallery 339, Philadelphia, PA. June 18- September 4, 2010.
+                    </p>
+                    <p>
+                    <em>Next: Emerging Philadelphia Photographers.</em> Philadelphia Photo Arts Center, Philadelphia, PA.August 18th- November 29th, 2009.
+                    </p>
+                    <h3>
+                        SECIAL COLLECTIONS
+                    </h3>
+                        <p>
+                        Madison Museum of Contemporary Art. 
+                        </p>
+                        <p>
+                        Three works in the permanent collection of Philadelphia Museum of Art 2011. MFA 
+                        </p>
+                        <p>
+                        Portfolio, thirteen works in the Yale University Robert B. Haas Family Arts Library 2014
+                        </p>
+                    <h3>
+                    FELLOWSHIP
+                    </h3>
+                    <p>
+                    National Trust of Historic Preservation & African American Cultural Heritage Fund. 2022 
+                    </p>
+                    <h3>
+                    AWARDS 
+                    </h3>
+                        <p>
+                        Richard Benson Prize, for excellence in photography. Yale School of Art 2014. 
+                        </p>
+                        <p>
+                        2nd Prize in the 2009 Philadelphia Photo Art Center juried exhibition NEXT, selected by Ariel  Shanbreg of the Center for Photography at Woodstock. 
+                        </p>
+                </div>
+            </div>
+
         </>
     )
 
