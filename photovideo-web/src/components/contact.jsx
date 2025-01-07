dac6aeebedb3734e32b7331e1825a2dfb32452c2
Hannahphoto <hannahprice67@gmail.com>
2025-01-06 19:09:33 -0500

    made book a carousel

--- photovideo-web/src/components/contact.jsx
+++ photovideo-web/src/components/contact.jsx
@@ -1,9 +1,25 @@
 
-export default function Contact(){
+export default function Contact() {
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText("hannahprice67@gmail.com").then(
+            () => {
+                alert("Email copied to clipboard!");
+            },
+            (err) => {
+                console.error("Failed to copy text: ", err);
+            }
+        );
+    };
 
     return (
-        <>
-        </>
-    )
-
+        <div style={{ textAlign: "center", marginTop: "20px" }}>
+            <h4 className="--bs-heading-color" style={{ color: "white", marginBottom: "10px" }}>
+                hannahprice67@gmail.com
+            </h4>
+            <button type="button" className="btn btn-outline-info" onClick={copyToClipboard} style={{ padding: "5px 10px", cursor: "pointer" }}>
+                Copy to Clipboard
+            </button>
+        </div>
+    );
 }
