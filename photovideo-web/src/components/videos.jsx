f6150823fd442b475e73e8685266e5afc517ea6e
Hannahphoto <hannahprice67@gmail.com>
2025-01-07 21:57:31 -0500

    videos embeded correctly

--- photovideo-web/src/components/videos.jsx
+++ photovideo-web/src/components/videos.jsx
@@ -13,29 +13,38 @@ export default function Videos() {
                 </h1>
             </div>
 
-            <div>
-
-                <div className="card w-50 p-3 text-bg-dark embed-responsive embed-responsive-16by9">
+            <div className="container">
+         {/* Video 1 */}
+         <div className="card mb-4">
+         <div className="ratio ratio-16x9">
                     <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/Mw1s6XuvwpM" allowFullScreen></iframe>
                 </div>
-
-
-
-                <div className="card w-50 p-3 text-bg-dark embed-responsive embed-responsive-16by9">
+                </div>
+ {/* Video 2 */}
+ <div className="card mb-4">
+         <div className="ratio ratio-16x9">
                     <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/97292707" allowFullScreen></iframe>
 
                 </div>
-
-                <div className="card w-50 p-3 text-bg-dark embed-responsive embed-responsive-16by9">
-                    <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/169453866" allowFullScreen></iframe>
                 </div>
 
-                <div className="card w-50 p-3 text-bg-dark embed-responsive embed-responsive-16by9">
+               {/* Video 3 */}
+         <div className="card mb-4">
+         <div className="ratio ratio-16x9">
+                    <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/169453866" allowFullScreen></iframe>
+                </div>
+                </div>
+
+
+                {/* Video 1 */}
+         <div className="card mb-4">
+         <div className="ratio ratio-16x9">
                     <iframe
                         className="embed-responsive-item"
                         src="https://www.youtube.com/embed/example"
                         allowFullScreen
                     ></iframe>
+                </div>
                 </div>
             </div>
         </>
