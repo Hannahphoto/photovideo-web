e424346a46129d5d199e9b953bae53076a2d53d7
Hannahphoto <hannahprice67@gmail.com>
2025-01-07 21:48:59 -0500

    embeded videos but cant fix resolution

--- photovideo-web/src/components/videos.jsx
+++ photovideo-web/src/components/videos.jsx
@@ -1,37 +1,42 @@
 
 export default function Videos() {
 
-    const openLink = (url) => {
-        window.open(url, '_blank');
-    };
+    // const openLink = (url) => {
+    //     window.open(url, '_blank');
+    // };
 
     return (
         <>
-            <div>
-                <h1 className="--bs-heading-color"style={{ color: "white" }}>
+            <div >
+                <h1 className="--bs-heading-color" style={{ color: "white" }}>
                     Videos
                 </h1>
             </div>
-            <div className="row row-cols-1 row-cols-md-3 g-4">
-                <div className="card text-bg-dark embed-responsive embed-responsive-16by9">
-                <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/Mw1s6XuvwpM" allowFullScreen></iframe>
-              
-                </div>
-                <div className="card text-bg-dark embed-responsive embed-responsive-16by9">
-                 
-                    <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/97292707" allowFullScreen></iframe>
-                   
-                </div>
-                <div className="card text-bg-dark">
-                         <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/169453866" allowFullScreen></iframe>
-                </div>
-                <div className="card text-bg-dark">
-                    <img src="/images/narrativeattempt.png" className="card-img" alt="..."></img>
-                     
-                        <button type="button" className="btn btn-outline-info"style={{ color: "white" }} >Untitled, An Attempt At Narrative</button>
-                </div>
-           
 
+            <div>
+
+                <div className="card w-50 p-3 text-bg-dark embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/Mw1s6XuvwpM" allowFullScreen></iframe>
+                </div>
+
+
+
+                <div className="card w-50 p-3 text-bg-dark embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/97292707" allowFullScreen></iframe>
+
+                </div>
+
+                <div className="card w-50 p-3 text-bg-dark embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/169453866" allowFullScreen></iframe>
+                </div>
+
+                <div className="card w-50 p-3 text-bg-dark embed-responsive embed-responsive-16by9">
+                    <iframe
+                        className="embed-responsive-item"
+                        src="https://www.youtube.com/embed/example"
+                        allowFullScreen
+                    ></iframe>
+                </div>
             </div>
         </>
     )
